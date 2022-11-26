96dccdb47d77f6999da582169d7094f9f29335ff
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-11-26 08:51:07 +0000

    images found

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,15 +3,13 @@ import "../styles/Pokemon.css";
 
 const Pokemon = ({ pokemon }) => {
   console.log(pokemon);
-
   return (
     <div className="container">
       {pokemon.slice(0, 10).map((item) => {
-        const { name, url } = item;
         return (
           <div>
-            <p className="pokemon-name">{name}</p>
-            <img src={url} alt="" />
+            <p className="pokemon-name">{item.name}</p>
+            <img src={item.sprites.back_default} alt="" />
           </div>
         );
       })}
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "../styles/SearchBar.css";
 import Pokemon from "./Pokemon";
+import axios from "axios";
 
 const SearchBar = () => {
   const [text, setText] = useState();
   const [pokemon, setPokemon] = useState([]);
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [nextUrl, setNextUrl] = useState();
+  const [prevUrl, setPrevUrl] = useState();
 
   const getPokemon = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    setPokemon(data);
+    // setPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.prev);
+    pokeFunction(data.results);
   };
 
-  const getImages = useEffect(() => {
+  const pokeFunction = async (data) => {
+    data.map(async (item) => {
+      const result = await axios.get(item.url);
+      setPokemon((state) => {
+        state = [...state, result.data];
+        return state;
+      });
+    });
+  };
+
+  useEffect(() => {
     getPokemon();
-  }, []);
+  }, [url]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
