94eeccdca93d2059617f91c3171309b6c00b5753
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-11-27 11:59:05 +0000

    css cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,12 +5,15 @@ const Pokemon = ({ pokemon }) => {
   console.log(pokemon);
   return (
     <div className="container">
-      {pokemon.slice(0, 10).map((item) => {
+      {pokemon.slice(0, 8).map((item) => {
         return (
           <div className="pokemon-container">
-            <p className="pokemon-name">{item.name}</p>
+            <p className="pokemon-name">
+              {item.name.charAt(0).toUpperCase() + item.name.slice(1)}
+            </p>
             <img src={item.sprites.back_shiny} alt="" />
-            <p className="height">{item.height}</p>
+            <p className="attack">Attack: {item.stats[0].base_stat}</p>
+            <p className="moves">Moves: {item.moves[0].move.name}</p>
           </div>
         );
       })}
